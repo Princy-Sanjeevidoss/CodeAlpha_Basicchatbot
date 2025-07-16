bf15ca4100588dc667f6a8717796dfcfcca80ef3
Princy <princysanjeevidoss@gmail.com>
2025-07-16 10:20:33 +0530

    modified

--- Basicchatbot.py
+++ Basicchatbot.py
@@ -1,1 +1,19 @@
+def chatbot():
+    print("Welcome! I'm a simple chatbot. Type 'bye' to exit.")
+
+    while True:
+        user_input = input("You: ").lower()
+
+        if user_input == "hello":
+            print("Bot: Hi!")
+        elif user_input == "how are you?":
+            print("Bot: I'm fine, thanks!")
+        elif user_input == "bye":
+            print("Bot: Goodbye!")
+            break
+        else:
+            print("Bot: Sorry, I didn't understand that.")
+
+
+chatbot()
 
